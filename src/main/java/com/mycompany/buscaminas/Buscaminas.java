4023ec79e96c295224b3fe62dc81e40dec62560a
Julian <est.juliane.salazar@unimilitar.edu.co>
2026-03-24 08:42:06 -0500

    Avanze del Proyecto Buscaminas Repositorio 3

--- src/main/java/com/mycompany/buscaminas/Buscaminas.java
+++ src/main/java/com/mycompany/buscaminas/Buscaminas.java
@@ -6,8 +6,15 @@ package com.mycompany.buscaminas;
 
 public class Buscaminas {
 
-    public static void main(String[] args) {
-        JuegoBuscaminas juego = new JuegoBuscaminas();
-        juego.iniciar();
+     public static void main(String[] args) {
+         
+         String nombre = Input.next("Ingrese su nombre: ");
+         Jugador jugador  = new Jugador(nombre);
+         
+         Tablero tablero = new Tablero(5, 5);
+         Juego juego = new Juego(tablero, jugador);
+         
+         juego.iniciarJuego();
+ 
     }
 }
