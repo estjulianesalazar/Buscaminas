e03497de38f5e1b7e7720a53231b0ddedc559ef0
Julian <est.juliane.salazar@unimilitar.edu.co>
2026-04-05 20:49:09 -0500

    Se cambio la clase Buscaminas en base a el menu y las opciones del jugador dentro del juego

--- src/main/java/com/mycompany/buscaminas/Buscaminas.java
+++ src/main/java/com/mycompany/buscaminas/Buscaminas.java
@@ -3,18 +3,44 @@
  */
 
 package com.mycompany.buscaminas;
+import com.murcia.utils.*;
 
 public class Buscaminas {
-
+    
      public static void main(String[] args) {
+     
+         String nombre = Input.next("Nombre: ");
+         Jugador jugador = new Jugador(nombre);
          
-         String nombre = Input.next("Ingrese su nombre: ");
-         Jugador jugador  = new Jugador(nombre);
-         
-         Tablero tablero = new Tablero(5, 5);
+         Tablero tablero = new Tablero(5, 5, 5);
          Juego juego = new Juego(tablero, jugador);
          
-         juego.iniciarJuego();
- 
-    }
+         String[] opciones = {
+           "Descubrir",
+           "Colocar Bandera",
+           "Quitar Bandera",
+           "Salir"
+         };
+         
+         Menu menu = new Menu(opciones, '>', "BuUSCAMINAS", "----");
+         
+         boolean salir = false;
+         
+         while (!salir){
+             
+             tablero.mostrarTablero();
+             juego.estadoJuego();
+             
+             char op = menu.select("Opcion:");
+             
+             int f, c;
+             
+             switch (op){
+                 
+                 case '1':
+                     f = Input.nextInt("Fila:", 0, 4);
+                     c = Input.nextInt("Columna:", 0, 4);
+             }
+         }
+     }
 }
